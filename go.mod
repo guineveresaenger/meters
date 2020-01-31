671ec4faa341c20296d9c0c8743599cb8ae2b7d9
guineveresaenger <guineveresaenger@github.com>
2020-01-30 17:09:43 -0800

    implemented GetMetricByName

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.13
 
 require (
 	github.com/kubernetes-incubator/custom-metrics-apiserver v0.0.0
-	k8s.io/apimachinery v0.17.2 // indirect
+	k8s.io/apimachinery v0.0.0-20190817020851-f2f3a405f61d
+	k8s.io/metrics v0.0.0-20190817023635-63ee757b2e8b
 )
 
 replace github.com/kubernetes-incubator/custom-metrics-apiserver => ../../../kubernetes-incubator/custom-metrics-apiserver
--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -3,10 +3,18 @@ package provider
 
 import (
 	"github.com/kubernetes-incubator/custom-metrics-apiserver/pkg/provider"
+	"github.com/kubernetes-incubator/custom-metrics-apiserver/pkg/provider/helpers"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/metrics/pkg/apis/custom_metrics"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	apimeta "k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"time"
 )
 
 type iceCreamMetricsProvider struct {
+	mapper apimeta.RESTMapper
 
 }
 
@@ -30,3 +38,26 @@ func (ic *iceCreamMetricsProvider) ListAllMetrics() []provider.CustomMetricInfo 
 		},
 	}
 }
+
+func (ic *iceCreamMetricsProvider) GetMetricByName(name types.NamespacedName, info provider.CustomMetricInfo) (*custom_metrics.MetricValue, error) {
+	var val int64
+	val = 1
+
+	objRef, err := helpers.ReferenceFor(ic.mapper, name, info)
+	if err != nil {
+		return nil, err
+	}
+
+	cmv := &custom_metrics.MetricValue{
+		DescribedObject: objRef,
+		Metric: custom_metrics.MetricIdentifier{
+			Name: info.Metric,
+		},
+		Timestamp: metav1.Time{time.Now()},
+		Value: *resource.NewQuantity(val, resource.DecimalSI),
+
+	}
+	return cmv, nil
+}
+
+
